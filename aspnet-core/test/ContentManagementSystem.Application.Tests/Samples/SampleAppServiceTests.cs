6b51d4b5301e80586b3a27d720a2a5e8bc6d199c
Roberto Arguello <rarguello@applaudostudios.com>
2024-01-11 04:26:21 -0600

    adding unit tests

--- aspnet-core/test/ContentManagementSystem.Application.Tests/Samples/SampleAppServiceTests.cs
+++ aspnet-core/test/ContentManagementSystem.Application.Tests/Samples/SampleAppServiceTests.cs
@@ -1,5 +1,8 @@
-﻿using Shouldly;
+﻿using ContentManagementSystem.Contents;
+using Shouldly;
+using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
 using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
 using Xunit;
@@ -15,10 +18,12 @@ public abstract class SampleAppServiceTests<TStartupModule> : ContentManagementS
     where TStartupModule : IAbpModule
 {
     private readonly IIdentityUserAppService _userAppService;
+    private readonly IContentAppService _contentAppService;
 
     protected SampleAppServiceTests()
     {
         _userAppService = GetRequiredService<IIdentityUserAppService>();
+        _contentAppService = GetRequiredService<IContentAppService>();
     }
 
     [Fact]
@@ -30,5 +35,64 @@ public abstract class SampleAppServiceTests<TStartupModule> : ContentManagementS
         //Assert
         result.TotalCount.ShouldBeGreaterThan(0);
         result.Items.ShouldContain(u => u.UserName == "admin");
+    }
+
+    [Fact]
+    public async Task CreateContent_ShouldSucceed()
+    {
+        //Act
+        var createDto = new CreateUpdateContentDto() { Name = "TestName", HtmlContent = ConvertTextToBytes("TestHtmlContent") };
+        var createResult = await _contentAppService.CreateAsync(createDto);
+
+        //Assert
+        createResult.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public async Task UpdateContent_ShouldSucceed()
+    {
+        //Act
+        var createDto = new CreateUpdateContentDto() { Name = "TestName", HtmlContent = ConvertTextToBytes("TestHtmlContent") };
+        var createResult = await _contentAppService.CreateAsync(createDto);
+
+        var updateDto = new CreateUpdateContentDto() { Name = "TestNameUpdated", HtmlContent = ConvertTextToBytes("TestHtmlContentUpdated") };
+        var updateResult = await _contentAppService.UpdateAsync(createResult.Id, createDto);
+
+        //Assert
+        updateResult.Name.Equals("TestNameUpdated");
+    }
+
+    [Fact]
+    public async Task GetContent_ShouldSucceed()
+    {
+        //Act
+        var createDto = new CreateUpdateContentDto() { Name = "TestName", HtmlContent = ConvertTextToBytes("TestHtmlContent") };
+        var createResult = await _contentAppService.CreateAsync(createDto);
+
+        createDto.Guid.ShouldBeNull();
+        var getResutl = await _contentAppService.GetAsync(createResult.Id);
+
+        //Assert
+        getResutl.Name.Equals("TestName");
+    }
+
+    [Fact]
+    public async Task GetListContent_ShouldSucceed()
+    {
+        //Act
+        var createDto = new CreateUpdateContentDto() { Name = "TestName", HtmlContent = ConvertTextToBytes("TestHtmlContent") };
+        await _contentAppService.CreateAsync(createDto);
+
+        var input = new PagedAndSortedResultRequestDto();
+        var getListResult = await _contentAppService.GetListAsync(input);
+
+        //Assert
+        getListResult.TotalCount.Equals(1);
+    }
+
+    static byte[] ConvertTextToBytes(string text)
+    {
+        Encoding encoding = Encoding.UTF8;
+        return encoding.GetBytes(text);
     }
 }
