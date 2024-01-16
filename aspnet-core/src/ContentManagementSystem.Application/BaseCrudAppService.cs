c526cc7a6461950c05815f118f1ccced6c9b8e13
ar9uello <rarguello@gmail.com>
2024-01-16 16:15:35 -0600

    adding cache to GetAsync, returning 404 in Get and Delete when id does not exist

---
+++ aspnet-core/src/ContentManagementSystem.Application/BaseCrudAppService.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Caching;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace ContentManagementSystem;
+
+public abstract class BaseCrudAppService<TEntity, TEntityDto, TKey, TGetListInput, TCreateInput>
+    : CrudAppService<TEntity, TEntityDto, TKey, TGetListInput, TCreateInput, TCreateInput>
+    where TEntity : class, IEntity<TKey>
+    where TEntityDto : class, IEntityDto<TKey>
+{
+    private readonly IDistributedCache<TEntityDto> _cache;
+
+    protected BaseCrudAppService(IRepository<TEntity, TKey> repository, IDistributedCache<TEntityDto> cache)
+        : base(repository)
+    {
+        _cache = cache;
+    }
+
+    public override async Task<TEntityDto> GetAsync(TKey id)
+    {
+        var cacheKey = $"{nameof(TEntity)}_{id}";
+
+        var cachedEntity = await _cache.GetAsync(cacheKey);
+        if (cachedEntity != null)
+        {
+            return cachedEntity;
+        }
+
+        var entity = await Repository.GetAsync(id) ?? throw new EntityNotFoundException(typeof(TEntityDto), id);
+        var dto = ObjectMapper.Map<TEntity, TEntityDto>(entity);
+        await _cache.SetAsync(cacheKey, dto);
+
+        return dto;
+    }
+
+    public override async Task DeleteAsync(TKey id)
+    {
+        _ = await Repository.GetAsync(id) ?? throw new EntityNotFoundException(typeof(TEntityDto), id);
+        await base.DeleteAsync(id);
+    }
+
+}
--- aspnet-core/src/ContentManagementSystem.Application/Contents/ContentAppService.cs
+++ aspnet-core/src/ContentManagementSystem.Application/Contents/ContentAppService.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using Volo.Abp;
 using Volo.Abp.Application.Dtos;
-using Volo.Abp.Application.Services;
+using Volo.Abp.Caching;
 using Volo.Abp.Domain.Repositories;
 
 namespace ContentManagementSystem.Contents
 {
     [RemoteService(false)]
-    public class ContentAppService(IRepository<Content, Guid> repository) : 
-        CrudAppService<
+    public class ContentAppService(IRepository<Content, Guid> repository, IDistributedCache<ContentDto> cache) : 
+        BaseCrudAppService<
             Content,
             ContentDto,
             Guid,
             PagedAndSortedResultRequestDto,
-            CreateUpdateContentDto>(repository), 
+            CreateUpdateContentDto>(repository, cache), 
         IContentAppService
     {
     }
